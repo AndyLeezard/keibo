5c632a54320d561797a6a555e3e189b563f796a9
Andy Lee <a.lee.intl@gmail.com>
2022-07-04 02:20:09 +0200

    iap_version_update

--- react_native/App.tsx
+++ react_native/App.tsx
@@ -1,13 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React, {type PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -19,13 +9,7 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import IAPManager from './src/lib/IAPManager';
 
 const Section: React.FC<
--- react_native/ios/keiboTests/keiboTests.m
+++ react_native/ios/keiboTests/keiboTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface keiboTests : XCTestCase
+@interface KeiboTests : XCTestCase
 
 @end
 
-@implementation keiboTests
+@implementation KeiboTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
