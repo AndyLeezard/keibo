4a7b0a07786f417674f643bb8f151b701093d353
Andy Lee <a.lee.intl@gmail.com>
2022-07-23 21:23:16 +0200

    test_firebase

--- webapp/pages/index.tsx
+++ webapp/pages/index.tsx
@@ -1,6 +1,21 @@
 import { NextPage } from "next"
+import { useEffect } from "react"
+import { doc, getDoc } from "firebase/firestore"
+import { firestore } from "../firebase"
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    const docRef = doc(firestore, "env", "vars")
+    getDoc(docRef).then((docSnap) => {
+      if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data())
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!")
+      }
+    })
+  }, [])
+
   return (
     <h1 style={{ textAlign: "center" }}>{`AYO LET'S BUILD THIS THING 🚀`}</h1>
   )
