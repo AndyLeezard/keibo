c9f18db1cc0d7f7b66bb469f17942fb601562a10
Andy Lee <a.lee.intl@gmail.com>
2022-07-23 21:07:12 +0200

    use_firebase

--- webapp/next.config.js
+++ webapp/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  env: {
+    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+    FIREBASE_AUTHDOMAIN: process.env.FIREBASE_AUTHDOMAIN,
+    FIREBASE_PROJECTID: process.env.FIREBASE_PROJECTID,
+    FIREBASE_STORAGEBUCKET: process.env.FIREBASE_STORAGEBUCKET,
+    FIREBASE_MESSAGINGSENDERID: process.env.FIREBASE_MESSAGINGSENDERID,
+    FIREBASE_APPID: process.env.FIREBASE_APPID,
+    FIREBASE_MID: process.env.FIREBASE_MID,
+  },
 }
 
 module.exports = nextConfig
