6685ca6d1b2a7ddaf6daa49a723111f251aabe1b
Andy Lee <a.lee.intl@gmail.com>
2022-07-23 21:08:15 +0200

    prop_bugfix_react_property_syntax

--- webapp/components/layout/header/widgets/ThemeButton.tsx
+++ webapp/components/layout/header/widgets/ThemeButton.tsx
@@ -34,9 +34,9 @@ const ThemeButton = ({ width, height, clickCallback }: ThemeButton) => {
         height={iconScale}
         viewBox="0 0 24 24"
         fill="none"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <path
           pathLength="1"
