11a4af42ff2392c0e43b83de091580555aeb2b3f
Andy Lee <a.lee.intl@gmail.com>
2022-07-31 23:15:19 +0200

    error_handler

--- webapp/components/signinContainer/signInContainer.tsx
+++ webapp/components/signinContainer/signInContainer.tsx
@@ -1,13 +1,15 @@
 import { ClientSafeProvider, signIn } from "next-auth/react"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Image from "next/image"
 import {
   h2r,
   keibo_accent_1,
   keibo_accent_2,
   keibo_accent_3,
+  keibo_crimson,
 } from "../../lib/palette"
 import styles from "./styles.module.css"
+import router, { useRouter } from "next/router"
 
 type SignInContainerProps = {
   providers: ClientSafeProvider[]
@@ -16,6 +18,14 @@ type SignInContainerProps = {
 const SignInContainer = ({ providers }: SignInContainerProps) => {
   const [hoverID, setHoverID] = useState<string>("")
   const [agreed, setAgreed] = useState(false)
+  const { error } = useRouter().query
+
+  useEffect(() => {
+    if (!agreed && error) {
+      setAgreed(true)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error])
 
   return (
     <div
@@ -24,16 +34,36 @@ const SignInContainer = ({ providers }: SignInContainerProps) => {
         backgroundColor: keibo_accent_1,
       }}
     >
-      <div className={styles.logo}>
+      <div className={styles.logo} onClick={() => router.push("/")}>
         <Image src={`/letter_64.png`} alt={"Keibo"} width={24} height={24} />
         <span>Keibo</span>
       </div>
+      {error ? (
+        <div
+          className={styles.error_message}
+          style={{
+            color: keibo_crimson,
+          }}
+        >
+          <span>
+            {error === "OAuthAccountNotLinked"
+              ? "The same email address has been already used via another authentification provider. To confirm your identity, sign in with the same account you used originally."
+              : error}
+          </span>
+        </div>
+      ) : (
+        <></>
+      )}
       <div className={styles.conditions}>
         <div
           className={`${styles.checkbox}`}
           style={{
             border: `2px solid ${agreed ? "transparent" : "#ffffff"}`,
-            backgroundColor: agreed ? keibo_accent_3 : "rgba(0, 0, 0, 0.5)",
+            backgroundColor: agreed
+              ? keibo_accent_3
+              : hoverID === "checkbox"
+              ? "rgba(255, 255, 255, 0.5)"
+              : "rgba(0, 0, 0, 0.5)",
             zIndex: 1,
           }}
           onMouseEnter={() => setHoverID("checkbox")}
@@ -41,12 +71,9 @@ const SignInContainer = ({ providers }: SignInContainerProps) => {
           onClick={() => setAgreed((prev) => !prev)}
         >
           <div
-            className={styles.checkbox_halo}
-            style={{
-              backgroundColor: `rgba(255, 255, 255, ${
-                hoverID === "checkbox" ? "0.25" : "0"
-              })`,
-            }}
+            className={`${styles.checkbox_halo} ${
+              agreed ? "" : styles.checkbox_halo_animated
+            }`}
           />
           {agreed ? (
             <Image
--- webapp/lib/palette.ts
+++ webapp/lib/palette.ts
@@ -8,6 +8,8 @@ export const keibo_accent_2 = "#517fe2"
 export const keibo_accent_3 = "#15cdcb"
 export const keibo_accent_4 = "#4fe0b5"
 
+export const keibo_crimson = "#ff6464"
+
 export const h2r = (hex: string, alpha = 1) => {
   const hexToArray = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   const result = hexToArray
